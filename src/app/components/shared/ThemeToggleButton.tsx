155b415e84356311ebcca02b66073dd26002ba86
oneminch <27751688+oneminch@users.noreply.github.com>
2024-07-09 18:51:00 -0400

    feat: implement theme toggle functionality

---
+++ src/app/components/shared/ThemeToggleButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Icon } from "@iconify-icon/react";
+import { useTheme } from "next-themes";
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const handleThemeToggle = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <div>
+      <button
+        type="button"
+        className="action-item w-9"
+        onClick={handleThemeToggle}
+        aria-label="Toggle Theme"
+      >
+        <Icon
+          icon={
+            theme === "dark"
+              ? "heroicons:moon-20-solid"
+              : "heroicons:sun-20-solid"
+          }
+        />
+      </button>
+    </div>
+  );
+}

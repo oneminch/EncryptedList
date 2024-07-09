e3d3eaaf5711d1f59c2bc594721c18c910b6d86c
oneminch <27751688+oneminch@users.noreply.github.com>
2024-07-09 18:40:35 -0400

    feat: create custom hooks for handling search & suggestions

---
+++ src/app/hooks/useSearch.ts
@@ -0,0 +1,16 @@
+import useSWR from "swr";
+import { fetcher } from "@/app/lib/products";
+
+export default function useSearch(query: string) {
+  const { data, error, isLoading } = useSWR<{ recipes: any[] }>(
+    `https://dummyjson.com/recipes/search?q=${query}&limit=10`,
+    fetcher
+  );
+
+  return {
+    products: data ? data.recipes : [],
+    isEmpty: data?.recipes?.length === 0,
+    isError: !!error,
+    isLoading
+  };
+}

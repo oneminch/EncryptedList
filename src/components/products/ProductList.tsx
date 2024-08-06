56b408a46676b2acc82f0c09606cbcfa9f8f1cf8
oneminch <27751688+oneminch@users.noreply.github.com>
2024-08-06 16:28:08 -0400

    refactor: extract fetching logic from product list component into home page

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -1,26 +1,21 @@
-import { queryProducts } from "@/lib/products";
 import ProductListItem from "./ProductListItem";
 
 export default async function ProductList({
-  sort,
-  tags,
-  query,
-  currentPage
+  total,
+  products
 }: {
-  sort: string;
-  tags: string[];
-  query: string;
-  currentPage: number;
+  total: number;
+  products: any;
 }) {
-  const products = await queryProducts({ sort, tags, query, currentPage });
-
-  // console.log("From ProductList", tags, sort, query, currentPage);
-
   return (
-    <ul className="border border-zinc-200 dark:border-transparent md:dark:border-zinc-700 bg-white dark:bg-zinc-900 flex flex-col px-5 py-4 rounded-lg">
-      {products?.recipes?.map((item: any) => (
-        <ProductListItem product={item} key={item} />
-      ))}
-    </ul>
+    <>
+      <ul className="border border-zinc-200 dark:border-transparent md:dark:border-zinc-700 bg-white dark:bg-zinc-900 flex flex-col px-5 py-4 rounded-lg">
+        {products?.map((item: any) => (
+          <ProductListItem product={item} key={item} />
+        ))}
+      </ul>
+      {/* Pagination */}
+      <p>{total}</p>
+    </>
   );
 }

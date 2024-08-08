3df3eced51d9fd5421e866aeb547ed5a64aecc46
oneminch <27751688+oneminch@users.noreply.github.com>
2024-08-08 16:43:43 -0400

    refactor: change data fetching logic to use API route handlers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,46 @@ import ProductList from "@/components/products/ProductList";
 import ProductListSkeleton from "@/components/products/ProductListSkeleton";
 import type { Metadata } from "next";
 import { metadata as pageMeta } from "@/lib/metadata";
+import type { QueryParams } from "@/lib/types";
+import { stringifySearchParams } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: { absolute: pageMeta["/"].title },
   description: pageMeta["/"].description
 };
 
-export default function Page({
+async function getProducts(params: string) {
+  const fetchParams = params.length > 0 ? `?${params}` : "";
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/products${fetchParams}`;
+
+  try {
+    const res = await fetch(apiUrl);
+
+    if (!res.ok) {
+      throw new Error("Failed to Fetch Products");
+    }
+
+    return res.json();
+  } catch (error) {
+    return error;
+  }
+}
+
+export default async function HomePage({
   searchParams
 }: {
-  searchParams?: {
-    query?: string;
-    page?: string;
-    sort?: string;
-    tags?: string;
-  };
+  searchParams: QueryParams;
 }) {
-  const query = searchParams?.query || "";
-  const sort = searchParams?.sort || "";
-  const page = Number(searchParams?.page) || 1;
-  const tags = searchParams?.tags?.split(",") || [];
+  const urlSearchParams = stringifySearchParams(searchParams);
+
+  const { products, total } = await getProducts(urlSearchParams);
 
   return (
     <div className="md:grid md:grid-cols-[16rem_minmax(240px,_1fr)] lg:grid-cols-[16rem_minmax(360px,_1fr)] md:gap-2 space-y-2 md:space-y-0">
       <Filter className="hidden md:flex" />
 
-      <Suspense key={query + page} fallback={<ProductListSkeleton />}>
-        <ProductList query={query} sort={sort} currentPage={page} tags={tags} />
+      <Suspense key={searchParams.sort} fallback={<ProductListSkeleton />}>
+        <ProductList total={total} products={products} />
       </Suspense>
     </div>
   );
--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,25 +1,43 @@
-import { Arguments } from "swr";
+import { QueryParams } from "./types";
 
 export const queryProducts = async ({
-  sort,
-  tags,
+  page,
   query,
-  currentPage
-}: {
-  sort: string;
-  tags: string[];
-  query: string;
-  currentPage: number;
-}): Promise<any> => {
-  return fetch(
-    "https://dummyjson.com/recipes?limit=10&select=name,instructions,tags,image"
-  ).then((res) => res.json());
+  sort,
+  tags
+}: QueryParams): Promise<any> => {
+  const perPage = 5;
+  const baseUrl = `https://dummyjson.com/recipes`;
+  const searchParams = new URLSearchParams();
+
+  searchParams.append("limit", perPage.toString());
+  searchParams.append("select", "name,instructions,tags,image");
+
+  if (sort) {
+    searchParams.append("sortBy", "name");
+    searchParams.append("order", sort);
+  }
+
+  if (query) {
+    searchParams.append("search", query);
+  }
+
+  if (tags && tags.length > 0) {
+    searchParams.append("tags", tags.join(","));
+  }
+
+  if (page !== undefined) {
+    searchParams.append("page", page.toString());
+  }
+
+  const queryString = searchParams.toString();
+  const apiUrl = queryString ? `${baseUrl}?${queryString}` : baseUrl;
+  console.log(apiUrl);
+
+  return fetch(apiUrl).then((res) => res.json());
 };
 
 export const searchProducts = async (query: string): Promise<any> => {
-  // return fetch(
-  //   `https://dummyjson.com/recipes?limit=50&select=name,instructions,tags,image`
-  // ).then((res) => res.json());
   return fetch(`https://dummyjson.com/recipes/search?q=${query}&limit=20`).then(
     (res) => res.json()
   );

dde37ba23e41dd2b285169a917159ea54b4fe9f4
oneminch <27751688+oneminch@users.noreply.github.com>
2024-12-06 07:01:22 -0500

    fix: make padding consistent on tag container

--- src/components/products/product-filter.tsx
+++ src/components/products/product-filter.tsx
@@ -49,9 +49,8 @@ export default function Filter({
         className={`${
           isCollapsed ? "max-h-0 md:max-h-96 hidden md:block" : "block max-h-96"
         } overflow-hidden md:overflow-visible transition-all duration-100 ease-linear space-y-4`}>
-        <div className="flex md:flex-col flex-row md:items-start items-center flex-wrap p-1 md:p-0">
-          {tags && <TagList tags={tags} />}
-        </div>
+        {tags && <TagList tags={tags} />}
+
         <button
           className="w-full px-4 py-2 mt-auto h-9 font-medium flex items-center justify-center rounded-md border border-b-2 focus-visible:global-focus bg-yellow-500 text-zinc-800 border-zinc-800 dark:border-yellow-500 disabled:cursor-not-allowed disabled:bg-zinc-50 dark:disabled:bg-zinc-800 disabled:text-zinc-400 dark:disabled:text-zinc-500 disabled:border-zinc-200 dark:disabled:border-zinc-700 disabled:focus-visible:ring-0"
           type="submit"
@@ -75,7 +74,7 @@ function TagList({ tags }: { tags: string[] }): React.ReactNode {
     <div
       ref={containerRef}
       tabIndex={-1}
-      className="flex md:flex-col flex-row md:items-start items-center flex-wrap p-1 md:p-0">
+      className="flex md:flex-col flex-row md:items-start items-center flex-wrap">
       {tags.map((item, index) => (
         <TagItem
           key={item}
